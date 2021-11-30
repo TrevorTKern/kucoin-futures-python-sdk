a1408ed747bce924bc5ff5ac70d409c88a08e62b
Trevor Kern <39813567+TrevorTKern@users.noreply.github.com>
2021-11-30 12:44:32 -0500

    Add files via upload

---
+++ kucoin/client.py
@@ -0,0 +1,16 @@
+from kucoin.market.market import MarketData
+from kucoin.trade.trade import TradeData
+from kucoin.user.user import UserData
+from kucoin.ws_token.token import GetToken
+
+class User(UserData):
+    pass
+
+class Trade(TradeData):
+    pass
+
+class Market(MarketData):
+    pass
+
+class WsToken(GetToken):
+    pass
