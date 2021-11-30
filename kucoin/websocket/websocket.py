64c03ee90f68c6049c6cba0d0ac446cf0cacb5eb
Trevor Kern <39813567+TrevorTKern@users.noreply.github.com>
2021-11-30 12:46:57 -0500

    Add files via upload

---
+++ kucoin/websocket/websocket.py
@@ -0,0 +1,159 @@
+import asyncio
+import json
+import time
+import websockets
+from random import random
+from uuid import uuid4
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ConnectWebsocket:
+    MAX_RECONNECTS = 5
+    MAX_RECONNECT_SECONDS = 60
+
+    def __init__(self, loop, client, callback, private=False):
+        self._loop = loop
+        self._client = client
+        self._callback = callback
+        self._reconnect_num = 0
+        self._ws_details = None
+        self._connect_id = None
+        self._private = private
+        self._last_ping = None
+        self._socket = None
+        asyncio.ensure_future(self.run_forever(), loop=self._loop)
+        self._topics = []
+
+    @property
+    def topics(self):
+        return self._topics
+
+    async def _run(self, event: asyncio.Event):
+        keep_alive = True
+        self._last_ping = time.time()  # record last ping
+        self._ws_details = None
+        self._ws_details = self._client.get_ws_token(self._private)
+        logger.warning(self._ws_details)
+
+        async with websockets.connect(self.get_ws_endpoint(), ssl=self.get_ws_encryption()) as socket:
+            self._socket = socket
+            self._reconnect_num = 0
+
+            if not event.is_set():
+                await self.send_ping()
+                event.set()
+
+            while keep_alive:
+                if time.time() - self._last_ping > self.get_ws_pingtimeout():
+                    await self.send_ping()
+
+                try:
+                    _msg = await asyncio.wait_for(self._socket.recv(), timeout=self.get_ws_pingtimeout())
+                except asyncio.TimeoutError:
+                    logger.exception('TimeoutError')
+                    await self.send_ping()
+                except asyncio.CancelledError:
+                    logger.exception('CancelledError')
+                    await self._socket.ping()
+                else:
+                    try:
+                        msg = json.loads(_msg)
+                    except ValueError:
+                        logger.warning(_msg)
+                        pass
+                    else:
+                        await self._callback(msg)
+
+    def get_ws_endpoint(self):
+        if not self._ws_details:
+            raise Exception("Websocket details Error")
+        ws_connect_id = str(uuid4()).replace('-', '')
+        token = self._ws_details['token']
+        endpoint = self._ws_details['instanceServers'][0]['endpoint']
+        ws_endpoint = f"{endpoint}?token={token}&connectId={ws_connect_id}"
+        return ws_endpoint
+        
+    def get_ws_encryption(self):
+        if not self._ws_details:
+            raise Exception("Websocket details Error")
+        return self._ws_details['instanceServers'][0]['encrypt']
+
+    def get_ws_pingtimeout(self):
+        if not self._ws_details:
+            raise Exception("Websocket details Error")
+        _timeout = int(self._ws_details['instanceServers'][0]['pingTimeout'] / 1000) - 2
+        return _timeout
+
+    async def run_forever(self):
+        while True:
+            await self._reconnect()
+
+    async def _reconnect(self):
+        logger.warning('start reconnect')
+
+        self._reconnect_num += 1
+        reconnect_wait = self._get_reconnect_wait(self._reconnect_num)
+        logger.warning(f'asyncio sleep {reconnect_wait}')
+        await asyncio.sleep(reconnect_wait)
+        logger.warning(f'asyncio sleep ok {reconnect_wait}')
+        event = asyncio.Event()
+
+        tasks = {
+            asyncio.ensure_future(self._recover_topic_req_msg(event), loop=self._loop): self._recover_topic_req_msg,
+            asyncio.ensure_future(self._run(event), loop=self._loop): self._run
+        }
+
+        while set(tasks.keys()):
+            finished, pending = await asyncio.wait(tasks.keys(), return_when=asyncio.FIRST_EXCEPTION)
+            exception_occur = False
+            for task in finished:
+                if task.exception():
+                    exception_occur = True
+                    print("{} got an exception {}".format(task, task.exception()))
+                    for pt in pending:
+                        logger.warning(f'pending {pt}')
+                        try:
+                            pt.cancel()
+                        except asyncio.CancelledError:
+                            logger.exception('CancelledError ')
+                        logger.warning('cancel ok.')
+
+            if exception_occur:
+                break
+
+        logger.warning('_reconnect over.')
+
+    async def _recover_topic_req_msg(self, event):
+        logger.warning(f'recover topic event {self.topics} waiting')
+        await event.wait()
+        logger.warning(f'recover topic event {self.topics} done.')
+        for topic in self.topics:
+            await self.send_message({
+                'type': 'subscribe',
+                'topic': topic,
+                'response': True
+            })
+            logger.warning(f'{topic} OK')
+
+    def _get_reconnect_wait(self, attempts):
+        expo = 2 ** attempts
+        return round(random() * min(self.MAX_RECONNECT_SECONDS, expo - 1) + 1)
+
+    async def send_ping(self):
+        msg = {
+            'id': str(int(time.time() * 1000)),
+            'type': 'ping'
+        }
+        await self._socket.send(json.dumps(msg))
+        self._last_ping = time.time()
+
+    async def send_message(self, msg, retry_count=0):
+        if not self._socket:
+            if retry_count < 5:
+                await asyncio.sleep(1)
+                await self.send_message(msg, retry_count + 1)
+        else:
+            msg['id'] = str(int(time.time() * 1000))
+            msg['privateChannel'] = self._private
+            await self._socket.send(json.dumps(msg))
